f7d26a9a88fa3e61a93d959113f07748b864f435
Dxy <denight@qq.com>
2017-08-14 14:18:32 +0800

    [update] new template

--- template.js
+++ template.js
@@ -1,28 +1,24 @@
 /**
- * @leetcode #1
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+ * @leetcode #0
+ * @param {number} num
+ * @return {boolean}
  */
-var twoSum = function(nums, target) {
-    let result = []
-
-    nums.some((num, index, array) => {
-        let diff = target - num
-        let anotherIndex = array.indexOf(diff, index + 1)
-        return anotherIndex > -1 && result.push(index, anotherIndex)
-    })
-
-    return result
+var leetcode = function(num) {
+    return Boolean(num)
 };
 
-const mock = [
-    [[2,7,11,16],9],
-    [[2,2,11,16],4],
-    [[2,7,11,16],27],
-    [[2,7,11,16],18],
-]
+void function test(f){
+    const mock = [
+        [0],
+        [1],
+    ]
 
-mock.forEach(function(item, index, array) {
-    console.log(twoSum.apply(null,item))
-})
+    const render = function(val){
+        console.log(val)
+        console.log()
+    }
+
+    mock.forEach(function(item, index, array) {
+        render(f.apply(null,item))
+    })
+}(leetcode)
